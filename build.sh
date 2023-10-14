3ff9ff286fc596054331a5d6e99d1a0dc653bb46
粑屁 <147690638+878088@users.noreply.github.com>
2023-10-14 09:20:42 +0800

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-install_kernel() {
-    git clone https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git --branch linux-6.5.y &&
-    cd linux &&
-    git remote add google-bbr https://github.com/google/bbr.git &&
-    git fetch google-bbr &&
-    git checkout linux-6.5.y &&
-    echo "Merge remote-tracking branch 'google-bbr/v3' into linux-6.5.y" > .git/MERGE_MSG &&
+kernel() {
+    git clone https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git --branch linux-6.5.y 
+    cd linux 
+    git remote add google-bbr https://github.com/google/bbr.git 
+    git fetch google-bbr 
+    git checkout linux-6.5.y 
+    echo "Merge remote-tracking branch 'google-bbr/v3' into linux-6.5.y" > .git/MERGE_MSG 
     git merge
 }
 
-install_kernel
+kernel
