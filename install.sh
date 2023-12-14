0fc19c8480d05835d4095b081414691bffa20352
粑屁 <147690638+878088@users.noreply.github.com>
2023-12-14 18:56:55 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
 sudo apt-get update -y
-sudo apt-get install git build-essential libncurses-dev libssl-dev libelf-dev libc-dev bison bc flex rsync debhelper screen libstdc++6 jq -y
+sudo apt-get build-dep linux jq git -y
+sudo apt-get upgrade
