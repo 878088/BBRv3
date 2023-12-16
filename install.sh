6586405e8d563fdce53185fd22386de71055149d
粑屁 <147690638+878088@users.noreply.github.com>
2023-12-17 02:01:40 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 sudo apt-get update -y
-sudo apt-get build-dep linux jq git gcc -y
+sudo apt install -y xz-utils make gcc dpkg-dev bc kmod cpio libssl-dev jq git
+sudo apt-get build-dep linux -y
 sudo apt-get upgrade
